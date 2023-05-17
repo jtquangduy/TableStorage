3aedfd2352436d77f342801468ba566cf4583d29
Quang Duy <justinquangduy@gmail.com>
2023-05-17 15:01:57 +0700

    Delete and Update in Azure Table Storage

--- TableStorage/Program.cs
+++ TableStorage/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Azure;
 using Azure.Data.Tables;
+using Microsoft.VisualBasic;
 
 string connectionString = "DefaultEndpointsProtocol=https;AccountName=qdappstore;AccountKey=rLatxMbQZlxq71IBpc0byfktQ5V2BBuKq/j2+VknI5B9iVOO1FlHGZGt0jEj+21PvM2S8iWcNWmk+AStALXoRg==;EndpointSuffix=core.windows.net";
 string tableName = "Orders";
@@ -9,8 +10,11 @@ string tableName = "Orders";
 //AddEntity("O3", "Desktop", 70);
 //AddEntity("O4", "Laptop", 200);
 
-QueryEntity("Laptop");
+//QueryEntity("Laptop");
 
+//DeleteEntity("Laptop", "O4");
+//DeleteEntity("O4", "Laptop");
+Update("O4", "Laptop", 600);
 
 void AddEntity(string orderId,string category,int quantity)
 {
@@ -37,4 +41,25 @@ void QueryEntity(string category)
         Console.WriteLine("Order Id: {0}", item.RowKey);
         Console.WriteLine("Quantity: {0}", item.GetInt32("Quantity"));
     }
+}
+
+void DeleteEntity(string category,string orderId)
+{
+    TableClient tableClient = new TableClient(connectionString,tableName);
+
+    tableClient.DeleteEntity(category, orderId);
+    
+    QueryEntity(category);
+}
+
+void Update(string category,string orderID, int quantity)
+{
+    TableClient tableClient = new TableClient(connectionString,tableName);
+
+    TableEntity tableEntity = new TableEntity(orderID, category) {
+        {"Quantity",quantity } };
+
+    tableClient.UpsertEntity(tableEntity);
+
+    Console.WriteLine("Entity with Partition Key {0} and Row Key {1} Updated", category, orderID);
 }
