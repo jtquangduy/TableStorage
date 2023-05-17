f8f2cb692cea99049bbfa128ae300a4c61a5cc0b
Quang Duy <justinquangduy@gmail.com>
2023-05-17 14:41:14 +0700

    query in azure table

--- TableStorage/Program.cs
+++ TableStorage/Program.cs
@@ -1,12 +1,16 @@
-﻿using Azure.Data.Tables;
+﻿using Azure;
+using Azure.Data.Tables;
 
 string connectionString = "DefaultEndpointsProtocol=https;AccountName=qdappstore;AccountKey=rLatxMbQZlxq71IBpc0byfktQ5V2BBuKq/j2+VknI5B9iVOO1FlHGZGt0jEj+21PvM2S8iWcNWmk+AStALXoRg==;EndpointSuffix=core.windows.net";
 string tableName = "Orders";
 
-AddEntity("O1", "Mobile", 100);
-AddEntity("O2", "Laptop", 50);
-AddEntity("O3", "Desktop", 70);
-AddEntity("O4", "Laptop", 200);
+//AddEntity("O1", "Mobile", 100);
+//AddEntity("O2", "Laptop", 50);
+//AddEntity("O3", "Desktop", 70);
+//AddEntity("O4", "Laptop", 200);
+
+QueryEntity("Laptop");
+
 
 void AddEntity(string orderId,string category,int quantity)
 {
@@ -20,4 +24,17 @@ void AddEntity(string orderId,string category,int quantity)
     tableClient.AddEntity(keyValuePairs);
 
     Console.WriteLine("Add Entity with Order ID {0}, {1}, {2}",orderId, category, quantity);
+}
+
+void QueryEntity(string category)
+{
+    TableClient tableClient = new TableClient(connectionString,tableName);
+
+    Pageable<TableEntity> result = tableClient.Query<TableEntity>(entity=>entity.PartitionKey==category);
+
+    foreach (var item in result)
+    {
+        Console.WriteLine("Order Id: {0}", item.RowKey);
+        Console.WriteLine("Quantity: {0}", item.GetInt32("Quantity"));
+    }
 }
